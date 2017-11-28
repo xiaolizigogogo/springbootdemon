e77ab477d43729408f616b1bb2d9fa254c74a849
user.name <user.email>
2017-11-28 16:19:36 +0800

    123

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jason Song(song_s@ctrip.com)
+ */
+@Configuration
+@EnableApolloConfig(value = "application", order = 10)
+public class AppConfig {
+}
