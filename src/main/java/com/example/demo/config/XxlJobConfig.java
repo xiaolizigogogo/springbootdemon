6497a0875bdce12ea5ec5c004958d428becbffa5
unknown <9267435@qq.com>
2017-11-28 22:23:48 +0800

    commit

--- src/main/java/com/example/demo/config/XxlJobConfig.java
+++ src/main/java/com/example/demo/config/XxlJobConfig.java
@@ -17,22 +17,22 @@ public class XxlJobConfig {
     private Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
 
 
-    @Value("${xxl.job.admin.addresses}")
+    @Value("${spring.platform.task.job.admin.addresses}")
     private String addresses;
 
-    @Value("${xxl.job.executor.appname}")
+    @Value("${spring.platform.task.job.executor.appname}")
     private String appname;
 
-    @Value("${xxl.job.executor.ip}")
+    @Value("${spring.platform.task.job.executor.ip}")
     private String ip;
 
-    @Value("${xxl.job.executor.port}")
+    @Value("${spring.platform.task.job.executor.port}")
     private int port;
 
-    @Value("${xxl.job.executor.logpath}")
+    @Value("${xspring.platform.task.job.executor.logpath}")
     private String logpath;
 
-    @Value("${xxl.job.accessToken}")
+    @Value("${spring.platform.task.job.accessToken}")
     private String accessToken;
 
     @Bean(initMethod = "start", destroyMethod = "destroy")
